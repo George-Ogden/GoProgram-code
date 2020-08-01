ecd6258f5e7ccba2c3d37e9c29c0162ed2d56445
George-Ogden <George-Ogden@outlook.com>
2020-08-01 14:09:40 +0100

    Lesson 12

--- about/tutorials.php
+++ about/tutorials.php
@@ -1,1 +1,96 @@
-<!doctype html><html><head><title>Understanding Python</title><link rel=stylesheet href=tutorials.css><link rel=stylesheet href=../fonts.css><link rel=stylesheet href=../main.css><link rel=stylesheet href=../colours.css><?php include "../meta.php" ?><meta name=description content="A website with a wide variety of showcased programming projects, as well as python tutorials."><meta name=keywords content="code, programming, GoProgram, python, tutorials, learning"><meta name=viewport content="width=device-width,initial-scale=1"><meta name=subject content=programming></head><?php include "../header.php" ?><nav><ul><li><a href="https://www.youtube.com/playlist?list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D" target=_blank>Videos on YouTube</a></li> |<li><a href=https://github.com/George-Ogden/Understanding-Python target=_blank>Code on GitHub</a></li></ul></nav><section><div id=top><h1>Understanding Python</h1></div><div id=return><span class="tooltip bottom">Top of page</span><div class=container><a href=#><i class="fa fa-angle-double-up"></i></a></div></div><div class="lesson box colour-1"><div class=h2><a href="https://www.youtube.com/watch?v=a4wymMoq_kc&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 01 - First Steps</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/a4wymMoq_kc allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-2"><div class=h2><a href="https://www.youtube.com/watch?v=x2v-9i5wMjo&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 02 - Joke Machine</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/x2v-9i5wMjo allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-3"><div class=h2><a href="https://www.youtube.com/watch?v=CBTsxoVH0Ig&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 03 - Maths Made Easy</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/CBTsxoVH0Ig allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-4"><div class=h2><a href="https://www.youtube.com/watch?v=ktmeL6qhl0U&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 04 - Calculator</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/ktmeL6qhl0U allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-5"><div class=h2><a href="https://www.youtube.com/watch?v=vXQvsYYdJHQ&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 05 - Making Tea</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/vXQvsYYdJHQ allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-4"><div class=h2><a href="https://www.youtube.com/watch?v=29YefhmOlS0&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 06 - Bouncer</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/29YefhmOlS0 allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-3"><div class=h2><a href="https://www.youtube.com/watch?v=sl8dVzgb6y0&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 07 - Interesting Items</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/sl8dVzgb6y0 allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-2"><div class=h2><a href="https://www.youtube.com/watch?v=YPvvuzoTu7s&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 08 - Chopping and Changing</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/YPvvuzoTu7s allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-1"><div class=h2><a href="https://www.youtube.com/watch?v=x-NpFY6eZCQ&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 09 - Python Turtle</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/x-NpFY6eZCQ allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-2"><div class=h2><a href="https://www.youtube.com/watch?v=G0w5aR-bUEM&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 10 - N-agon</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/G0w5aR-bUEM allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div><div class="lesson box colour-3"><div class=h2><a href="https://www.youtube.com/watch?v=s39lQsbaGDA&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D"><h2>Lesson 11 - Guess My Number</h2></a></div><div class=vid><iframe src=https://youtube.com/embed/s39lQsbaGDA allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div></div></section><div id=cookie></div><?php include "../footer.php" ?><script src=../main.js></script><?php include "../scripts.php" ?></html>
+<!doctype html>
+<html>
+
+<head>
+    <title>Understanding Python</title>
+    <link rel=stylesheet href=tutorials.css>
+    <link rel=stylesheet href=../fonts.css> <link rel=stylesheet href=../main.css> <link rel=stylesheet href=../colours.css><?php include "../meta.php" ?> <meta name=description content="A website with a wide variety of showcased programming projects, as well as python tutorials.">
+    <meta name=keywords content="code, programming, GoProgram, python, tutorials, learning">
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <meta name=subject content=programming>
+</head><?php include "../header.php" ?><nav>
+    <ul>
+        <li><a href="https://www.youtube.com/playlist?list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D" target=_blank>Videos on YouTube</a></li> |<li><a href=https://github.com/George-Ogden/Understanding-Python target=_blank>Code on GitHub</a></li>
+    </ul>
+</nav>
+<section>
+    <div id=top>
+        <h1>Understanding Python</h1>
+    </div>
+    <div id=return><span class="tooltip bottom">Top of page</span>
+        <div class=container><a href=#><i class="fa fa-angle-double-up"></i></a></div>
+    </div>
+    <div class="lesson box colour-1">
+        <div class=h2><a href="https://www.youtube.com/watch?v=a4wymMoq_kc&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 01 - First Steps</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/a4wymMoq_kc allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-2">
+        <div class=h2><a href="https://www.youtube.com/watch?v=x2v-9i5wMjo&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 02 - Joke Machine</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/x2v-9i5wMjo allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-3">
+        <div class=h2><a href="https://www.youtube.com/watch?v=CBTsxoVH0Ig&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 03 - Maths Made Easy</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/CBTsxoVH0Ig allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-4">
+        <div class=h2><a href="https://www.youtube.com/watch?v=ktmeL6qhl0U&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 04 - Calculator</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/ktmeL6qhl0U allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-5">
+        <div class=h2><a href="https://www.youtube.com/watch?v=vXQvsYYdJHQ&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 05 - Making Tea</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/vXQvsYYdJHQ allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-4">
+        <div class=h2><a href="https://www.youtube.com/watch?v=29YefhmOlS0&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 06 - Bouncer</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/29YefhmOlS0 allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-3">
+        <div class=h2><a href="https://www.youtube.com/watch?v=sl8dVzgb6y0&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 07 - Interesting Items</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/sl8dVzgb6y0 allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-2">
+        <div class=h2><a href="https://www.youtube.com/watch?v=YPvvuzoTu7s&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 08 - Chopping and Changing</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/YPvvuzoTu7s allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-1">
+        <div class=h2><a href="https://www.youtube.com/watch?v=x-NpFY6eZCQ&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 09 - Python Turtle</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/x-NpFY6eZCQ allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-2">
+        <div class=h2><a href="https://www.youtube.com/watch?v=G0w5aR-bUEM&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 10 - N-agon</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/G0w5aR-bUEM allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-3">
+        <div class=h2><a href="https://www.youtube.com/watch?v=s39lQsbaGDA&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 11 - Guess My Number</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/s39lQsbaGDA allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+    <div class="lesson box colour-4">
+        <div class=h2><a href="https://www.youtube.com/watch?v=wm26TxP-4j0&list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D">
+                <h2>Lesson 12 - Randomness</h2>
+            </a></div>
+        <div class=vid><iframe src=https://youtube.com/embed/wm26TxP-4j0 allow="autoplay encrypted-media" allowfullscreen>Video Unavailable</iframe></div>
+    </div>
+</section>
+<div id=cookie></div><?php include "../footer.php" ?><script src=../main.js> </script><?php include "../scripts.php" ?> </html>

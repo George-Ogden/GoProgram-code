4b2e057150a963b1955339bb4b672c73f179065d
George Ogden <George-Ogden@outlook.com>
2020-09-18 11:39:08 +0100

    edited footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<hr class="footer"><footer><?php function icon($type)
+<footer class="bg-secondary"><?php function icon($type)
 {
 $lower = strtolower($type);
 echo "<div  class='$lower icon'><a href=/$lower target=_blank data-toggle=tooltip data-placement=top title=$type><img alt=$type class=img src=/images/$type.png></a></div>";

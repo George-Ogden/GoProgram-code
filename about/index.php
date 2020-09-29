422c402458e7dc2a0d4e8f6566e8d1d03fd36dfa
George Ogden <George-Ogden@outlook.com>
2020-09-29 09:54:59 +0100

    Added picture of myself

--- about/index.php
+++ about/index.php
@@ -15,8 +15,9 @@
 <h2>Welcome</h2>
 <p>Welcome to GoProgram, a website showcasing programming. Everything is coded from scratch and it is all entirely original. Enjoy browsing round the projects and using them, and feel free to use some of the code in your own projects if you wish!</p>
 </div>
-<?php echo image("images/code.jpg", "Computer Code", "text-darken", ["text" => "<h2>Programs</h2><p>With passions in maths and computer science, you will notice a wide range of mathematical concepts display throughout the projects on the website.</p>"])  ?>
-<?php echo image("../images/logos.jpg", "Hand Drawn Logos", "text-overlay", ["text" => "<h2>Skills</h2><p>These are the main skills that I am proficient in, in picture format. If you are looking for a skill that is not listed, feel free to contact me.</p>"]); ?>
+<?php ;echo section("George Ogden","I am an experienced freelance software and web developer with in-depth knowledge of multiple programming languages and paradigms. I have completed a wide variety of projects ranging from apps to artificial intelligence, from software to sudoku solvers, many of which are showcased right here on this website!","images/george ogden.jpg");
+echo image("images/code.jpg", "Computer Code", "text-darken", ["text" => "<h2>Programs</h2><p>With passions in maths and computer science, you will notice a wide range of mathematical concepts display throughout the projects on the website.</p>"]);
+echo image("../images/logos.jpg", "Hand Drawn Logos", "text-overlay", ["text" => "<h2>Skills</h2><p>These are the main skills that I am proficient in, in picture format. If you are looking for a skill that is not listed, feel free to contact me.</p>"]); ?>
 <div class="section text">
 <h2>Donate</h2>
 <p>The website costs money to run, as do all the open source projects that I work on and the weekly videos that I publish. These rely on an entirely donation-based system. Please, consider <a href="/patreon" target="blank" data-toggle="tooltip" title="Donate">donating via Patreon</a> to support the site as well as receiving exclusive benefits and early access.</p>

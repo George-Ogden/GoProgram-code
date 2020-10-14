bfcc1523e3f4c74c3bb4b11d07477fcb62d38ae8
George Ogden <George-Ogden@outlook.com>
2020-10-14 19:57:53 +0100

    addded media queries

--- artificial-intelligence/dinosaurs.php
+++ artificial-intelligence/dinosaurs.php
@@ -3,7 +3,7 @@
 <head>
 <title>Dinosaur Generator</title>
 <?php include "../main.php" ?>
-<link rel=stylesheet href=dinosaurs.css?v=1>
+<link rel=stylesheet href=dinosaurs.css?v=2>
 <?php include "../meta.php" ?>
 <meta name=description content="This is an interactive piece of AI that generates dinasaur names with the input.">
 <meta name=keywords content="color classifier">
@@ -23,9 +23,9 @@
 </div>
 <div class="section text pad-1" id="generator">
 <h2 class="w-100 mw-100">Generator</h2>
-<input id=text class="text mx-auto bg-white border border-dark my-5 text-center display-3">
-<p class="text-center display-3 w-100" id=output><span class="typed font-weight-bold"></span><span class="generated font-weight-light"></span></p>
+<input id=text class="text container mx-auto bg-white border border-dark my-5 text-center display-3">
+<p class="text-center display-3 container" id=output><span class="typed font-weight-bold"></span><span class="generated font-weight-light"></span></p>
 </div>
 <?php include "../footer.php"?></section><?php include "../scripts.php" ?>
-<script src=dinosaurs.js?v=1></script>
+<script src=dinosaurs.js?v=2></script>
 </html>

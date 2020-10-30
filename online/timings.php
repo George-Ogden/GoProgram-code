620741bee44a1c8bfafdcbcd07c1e2f7853c2003
George Ogden <George-Ogden@outlook.com>
2020-10-30 15:41:20 +0000

    enhancement

--- online/timings.php
+++ online/timings.php
@@ -24,21 +24,21 @@
 <div><input id=mi placeholder="Mile time" time=60></div>
 <h2>Custom Distance</h2>
 <div><select class="mdb-select md-form" id=distance>
-  <option value="" disabled>Distance</option>
-  <option value=100>100m</option>
-  <option value=200>200m</option>
-  <option value=300>300m</option>
-  <option value=800>800m</option>
-  <option value=1500 selected time=60>1500m</option>
-  <option value=3000 time=60>3000m / 3km</option>
-  <option value=5000 time=60>5000m / 5km</option>
-  <option value=10000 time=60>1000m / 10 km</option>
-  <option value=16093 time=60>10 miles</option>
-  <option value=21097 time=60>Half Marathon</option>
-  <option value=42195 time=3600>Marathon</option>
+<option value="" disabled>Distance</option>
+<option value=100m>100m</option>
+<option value=200m>200m</option>
+<option value=300m>300m</option>
+<option value=800m>800m</option>
+<option value=1500m time=60>1500m</option>
+<option value=3km time=60>3000m / 3 km</option>
+<option value=5km time=60>5000m / 5 km</option>
+<option value=10km time=60>1000m / 10 km</option>
+<option value=10-miles time=60>10 miles</option>
+<option value=half time=60>Half Marathon</option>
+<option value=marathon time=3600>Marathon</option>
 </select><input id=time placeholder="Time" time=60></div>
 </div>
 </div>
-<script src=timings.js></script>
+<script src=timings.js?v=2></script>
 <?php include "../footer.php" ?></section><?php include "../scripts.php" ?>
 </html>

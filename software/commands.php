8d4e6f3361e463aaaa74de8d14ea75f499227067
George Ogden <George-Ogden@outlook.com>
2020-11-15 14:00:20 +0000

    changed installer

--- software/commands.php
+++ software/commands.php
@@ -15,7 +15,7 @@
          <p>On Windows, the command line interface is very limited, especially when compared to other operating systems. This makes using it quite difficult, and this is even more challenging as the commands are often different between operating systems. The Coreutils, as well as other commands have been compiled on a Windows machine so that you can run these commands from the command line without using virtual machines or containers. It makes using the command line a lot easier.</p>
      </div>
      <?php
-        echo section("Installer", "You can <a href=/scripts/exe/birthday-reminder-setup.exe download>run the Windows installer</a> with a few options to customise the software for what you need. Additionally, it will add the program to the PATH environmental variable so that the tools are available from the command line.", "images/installer.jpg");
+        echo section("Installer", "You can <a href=/scripts/exe/unix-commands-setup.exe download>run the Windows installer</a> with a few options to customise the software for what you need. Additionally, it will add the program to the PATH environmental variable so that the tools are available from the command line.", "images/installer.jpg");
         ?>
  <?php include "../footer.php" ?></section><?php include "../scripts.php" ?>
  </html>

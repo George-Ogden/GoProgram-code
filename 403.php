362bf6d7b47befc9cd342055a39b0a44d6c9bdc9
George Ogden <George-Ogden@outlook.com>
2021-01-01 12:52:53 +0000

    fixed error pages

--- 403.php
+++ 403.php
@@ -5,11 +5,10 @@
 <?php include "main.php" ?>
 <link rel=stylesheet href=/400.css?v=2><meta name=viewport content="width=device-width,initial-scale=1">
 </head><?php include "header.php" ?><section>
-<h1 class="colour d-flex justify-content-center align-items-center h-100 pad-1"><span>4</span><?php echo image("/images/goprogram.png", "GoProgram Logo", "none", ["id" => "BG"]);
+<h1 class="colour d-flex justify-content-center align-items-center h-100 pad-1 loaded"><span>4</span><?php echo image("/images/goprogram.png", "GoProgram Logo", "none", ["id" => "BG"]);
 ?><span>3</span></h1>
 <div class="section text p-0 pb-5">
 <p class="text-center display-4">You're not allowed here!</p>
 </div>
-</section>
-<?php include "footer.php" ?><?php include "scripts.php" ?>
+<?php include "footer.php" ?></section><?php include "scripts.php" ?>
 </html>

fec2342b7b7e35a94585677d75b59c8a00f1cd32
George Ogden <George-Ogden@outlook.com>
2021-01-01 13:01:27 +0000

    fixed tooltip bug

--- 403.php
+++ 403.php
@@ -3,7 +3,7 @@
 <head>
 <title>403 | GoProgram</title>
 <?php include "main.php" ?>
-<link rel=stylesheet href=/400.css?v=2><meta name=viewport content="width=device-width,initial-scale=1">
+<link rel=stylesheet href=/400.css?v=3><meta name=viewport content="width=device-width,initial-scale=1">
 </head><?php include "header.php" ?><section>
 <h1 class="colour d-flex justify-content-center align-items-center h-100 pad-1 loaded"><span>4</span><?php echo image("/images/goprogram.png", "GoProgram Logo", "none", ["id" => "BG"]);
 ?><span>3</span></h1>

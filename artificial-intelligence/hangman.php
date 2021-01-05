e0d1f56af437c5ee06990dec423e9c88e3619ae0
George Ogden <George-Ogden@outlook.com>
2021-01-05 14:38:59 +0000

    finished hangman

--- artificial-intelligence/hangman.php
+++ artificial-intelligence/hangman.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<title>Hangman</title>
+<title>Hangman Solver</title>
 <?php include "../main.php" ?>
 <link rel=stylesheet href=hangman.css?v=1>
 <?php include "../meta.php" ?>
@@ -12,13 +12,37 @@
 </head><?php include "../header.php" ?>
 <section>
 <div class=section id=top>
-<h1 class="colour pad-1">Hangman</h1>
+<h1 class="colour pad-1">Hangman Solver</h1>
 </div>
-<form class="mb-5">
-</form>
-</div>
-<script src=hangman.js?v=1></script>
+<div class="section interactive" id=main>
+<div class="main mx-0 w-100">
+<p>Think of a word:</4>
+<h2 class=pad-1>Length</h2>
+<div class="main d-none"><div class="mx-auto pad-2 w-100"><p class=guess></p><div class=buttons></div><p class=info></p><button class="major done">Done</button></div></div>
+<select class="mdb-select md-form my-2 mx-auto" id=length>
+<?php for ($i = 4; $i <= 12; $i++){
+if ($i == 5){
+echo "<option value=$i selected>$i</option>";
+} else {
+echo "<option value=$i>$i</option>";
+}
+}
+?>
+</select>
+</div><button id=start class="major mt-5">Start</button></div>
+<script type="text/javascript" src=sdk/lib/axios/dist/axios.standalone.js></script>
+<script type="text/javascript" src=sdk/lib/cryptojs/rollups/hmac-sha256.js></script>
+<script type="text/javascript" src=sdk/lib/cryptojs/rollups/sha256.js></script>
+<script type="text/javascript" src=sdk/lib/cryptojs/components/hmac.js></script>
+<script type="text/javascript" src=sdk/lib/cryptojs/components/enc-base64.js></script>
+<script type="text/javascript" src=sdk/lib/url-template/url-template.js></script>
+<script type="text/javascript" src=sdk/lib/apigatewaycore/sigv4client.js></script>
+<script type="text/javascript" src=sdk/lib/apigatewaycore/apigatewayclient.js></script>
+<script type="text/javascript" src=sdk/lib/apigatewaycore/simplehttpclient.js></script>
+<script type="text/javascript" src=sdk/lib/apigatewaycore/utils.js></script>
+<script type="text/javascript" src=sdk/apigClient.js></script>
 <?php include "../footer.php" ?></section>
 <?php include "../scripts.php" ?>
+<script src=hangman.js?v=1></script>
 </script>
 </html>
---
+++ artificial-intelligence/sdk/apigclient.js
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+var apigClientFactory = {};
+apigClientFactory.newClient = function (config) {
+    var apigClient = {};
+    if (config === undefined) {
+        config = {
+            accessKey: "",
+            secretKey: "",
+            sessionToken: "",
+            region: "",
+            apiKey: undefined,
+            defaultContentType: "application/json",
+            defaultAcceptType: "application/json",
+        };
+    }
+    if (config.accessKey === undefined) {
+        config.accessKey = "";
+    }
+    if (config.secretKey === undefined) {
+        config.secretKey = "";
+    }
+    if (config.apiKey === undefined) {
+        config.apiKey = "";
+    }
+    if (config.sessionToken === undefined) {
+        config.sessionToken = "";
+    }
+    if (config.region === undefined) {
+        config.region = "us-east-1";
+    }
+    //If defaultContentType is not defined then default to application/json
+    if (config.defaultContentType === undefined) {
+        config.defaultContentType = "application/json";
+    }
+    //If defaultAcceptType is not defined then default to application/json
+    if (config.defaultAcceptType === undefined) {
+        config.defaultAcceptType = "application/json";
+    }
+
+    // extract endpoint and path from url
+    var invokeUrl = "https://31z0kcmst1.execute-api.eu-west-2.amazonaws.com/main";
+    var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
+    var pathComponent = invokeUrl.substring(endpoint.length);
+
+    var sigV4ClientConfig = {
+        accessKey: config.accessKey,
+        secretKey: config.secretKey,
+        sessionToken: config.sessionToken,
+        serviceName: "execute-api",
+        region: config.region,
+        endpoint: endpoint,
+        defaultContentType: config.defaultContentType,
+        defaultAcceptType: config.defaultAcceptType,
+    };
+
+    var authType = "NONE";
+    if (sigV4ClientConfig.accessKey !== undefined && sigV4ClientConfig.accessKey !== "" && sigV4ClientConfig.secretKey !== undefined && sigV4ClientConfig.secretKey !== "") {
+        authType = "AWS_IAM";
+    }
+
+    var simpleHttpClientConfig = {
+        endpoint: endpoint,
+        defaultContentType: config.defaultContentType,
+        defaultAcceptType: config.defaultAcceptType,
+    };
+
+    var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
+
+    apigClient.guess = function (clue, used) {
+        params = {
+            clue: clue.toLowerCase(),
+        };
+        if (used != "") {
+            params.used = used.toLowerCase();
+        }
+
+        var guessGetRequest = {
+            verb: "get".toUpperCase(),
+            path: pathComponent + uritemplate("/guess").expand(apiGateway.core.utils.parseParametersToObject(params, ["clue", "used"])),
+            headers: "",
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ["clue", "used"]),
+            body: "",
+        };
+
+        return apiGatewayClient.makeRequest(guessGetRequest, authType, {}, config.apiKey);
+    };
+
+    apigClient.update = function (word) {
+        params = {
+            word: word.toLowerCase(),
+        };
+
+        var updatePutRequest = {
+            verb: "put".toUpperCase(),
+            path: pathComponent + uritemplate("/update").expand(apiGateway.core.utils.parseParametersToObject(params, ["word"])),
+            headers: "",
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ["word"]),
+            body: "",
+        };
+
+        return apiGatewayClient.makeRequest(updatePutRequest, authType, {}, config.apiKey);
+    };
+
+    return apigClient;
+};

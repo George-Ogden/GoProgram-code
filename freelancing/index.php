3dbf8d39bbff0068da13376b2658b840991091b9
George Ogden <George-Ogden@outlook.com>
2021-01-22 18:30:10 +0000

    plan for new section

---
+++ freelancing/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<title>Freelancig</title>
+<?php include "../main.php" ?>
+<?php include "../meta.php" ?>
+<meta name=description content="George Ogden is a freelance software and web developer, who is always looking for new and interesting challenges.">
+<meta name=keywords content="software web developer freelance">
+<meta name=viewport content="width=device-width,initial-scale=1">
+<meta name=subject content=freelancing>
+</head><?php include "../header.php" ?>
+<section class="colour">
+<?php echo image("images/software-development.jpg", "Freelancing", "text-darken", ["id" => "top", "text" => "<h1>Freelancing</h1>"]);  ?>
+<div class="section text text-center">
+<p></p>
+</div>
+</div>
+<?php include "../footer.php" ?></section><?php include "../scripts.php" ?>
+</html>

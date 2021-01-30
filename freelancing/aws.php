2268e4236fe782a36ca55a9cc7e951515c208516
George Ogden <George-Ogden@outlook.com>
2021-01-30 15:05:09 +0000

    started aws page

---
+++ freelancing/aws.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<title>AWS</title>
+<?php include "../main.php" ?>
+<?php include "../meta.php" ?>
+<link rel=stylesheet href=aws.css?v=1>
+<meta name=description content="George Ogden is a freelance developer with two AWS certifications and he is always looking for new and interesting challenges.">
+<meta name=keywords content="aws developer">
+<meta name=viewport content="width=device-width,initial-scale=1">
+<meta name=subject content="aws freelance development">
+</head>
+<?php include "../header.php" ?>
+<section class="colour">
+<?php echo image("images/aws.jpg", "AWS", "text-darken", ["id" => "top", "text" => "<h1>AWS</h1>"]);?>
+
+<?php echo pph(); include "../footer.php" ?></section><?php include "../scripts.php" ?>
+</html>

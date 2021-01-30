6ffaa615e9bf3515fae9ae04d4b3f1c395717c73
George Ogden <George-Ogden@outlook.com>
2021-01-30 15:02:01 +0000

    changed image

--- freelancing/software.php
+++ freelancing/software.php
@@ -14,7 +14,7 @@
 <?php echo image("images/software.jpg", "Software Development", "text-darken", ["id" => "top", "text" => "<h1>Software Development</h1>"]);
 echo skills()?>
 <div class="section image" id=python><div class=text><h2 class=pb-5>Python</h2><p class=pb-5>Python is my main language for software development. I have an extensive knowledge of various frameworks that allows me to implement efficient and effective code to solve a variety of problems.</p></div>
-<?php echo image("images/python.jpg","Python logo","none") ?>
+<?php echo image("../images/python.jpg","Python logo","none") ?>
 </div>
 <div class="section image" id=code><div class=text><h2 class=pb-5>Code</h2><p class=pb-5>All code that I produce is commented and laid out in a clear manner with obvious meaning so that anyone can follow along. It is reusable and robust so that it can be used for many problems.</p></div>
 <?php echo image("images/python-code.jpg","Python code","none") ?>

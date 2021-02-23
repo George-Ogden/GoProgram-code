f75e74f1e75c1a323fcc7cab476d80871d6c5f3b
George Ogden <George-Ogden@outlook.com>
2021-02-23 15:29:06 +0000

    minor modifications

--- artificial-intelligence/external/yolo.php
+++ artificial-intelligence/external/yolo.php
@@ -10,11 +10,11 @@
 <meta name=viewport content="width=device-width,initial-scale=1">
 <meta name=subject content="YOLO implmentation">
 </head><?php include "../../header.php" ?>
-<section>
+<section class=colour>
 <div class=section id=top>
 <h1 class="colour pad-2 loaded text-left"><span class=highlight>Y</span>ou<br><span class=highlight>O</span>nly<br><span class=highlight>L</span>ook<br><span class=highlight>O</span>nce</h1>
 </div>
-<?php echo section("YOLO","YOLO is an artificial intelligence algorithm for object detection. As well as classification, YOLO identifies where in an image an object is. It is fast enough to be run in real-time and achieves very a high level of accuracy. The output is a series of bounding boxes with labelled classes, which identify eac object in the image.","/about/images/george ogden.jpg","about") . image("images/yolo.jpg","Custom Playing Card Detector","none") ?>
+<?php echo section("YOLO","YOLO is an artificial intelligence algorithm for object detection. As well as classification, YOLO identifies where in an image an object is. It is fast enough to be run in real-time and achieves very a high level of accuracy. The output is a series of bounding boxes with labelled classes, which identify each object in the image.","/about/images/george ogden.jpg","about") . image("images/yolo.jpg","Custom Playing Card Detector","none") ?>
 <div class="section icons">
 <a class="icon" href="https://github.com/pjreddie/darknet" target="_blank">
 <ion-icon name="logo-github"></ion-icon>

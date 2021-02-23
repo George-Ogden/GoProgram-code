1f3df4523a71188338d94fab5dc78ae4fcb262fd
George Ogden <George-Ogden@outlook.com>
2021-02-23 15:29:20 +0000

    updated musegan page

--- artificial-intelligence/external/musegan.php
+++ artificial-intelligence/external/musegan.php
@@ -10,19 +10,25 @@
 <meta name=viewport content="width=device-width,initial-scale=1">
 <meta name=subject content="YOLO implmentation">
 </head><?php include "../../header.php" ?>
-<section>
+<section class=colour>
 <div class=section id=top>
-<h1 class="colour pad-2 loaded text-left"><span class=highlight>Mu</span>lti-track<br><span class=highlight>se</span>equential<br><span class=highlight>G</span>enerative<br><span class=highlight>A</span>dversarial<br><span class=highlight>N</span>etwork</h1>
+<h1 class="colour pad-2 loaded text-left"><span class=highlight>Mu</span>lti-track<br><span class=highlight>se</span>quential<br><span class=highlight>G</span>enerative<br><span class=highlight>A</span>dversarial<br><span class=highlight>N</span>etwork</h1>
 </div>
-<?php echo section("YOLO","YOLO is an artificial intelligence algorithm for object detection. As well as classification, YOLO identifies where in an image an object is. It is fast enough to be run in real-time and achieves very a high level of accuracy. The output is a series of bounding boxes with labelled classes, which identify eac object in the image.","/about/images/george ogden.jpg","about") . image("images/yolo.jpg","Custom Playing Card Detector","none") ?>
+<div class=section>
+<div class="section text"><h2>MuseGAN</h2><p>MuseGAN is an artificial intelligence model that generates music with multiple instruments. It is able to generate music either from scratch or with an accompanying track. I trained it on songs by The Beatles, and, in the processes, made contributions to the original project in the form data preparation scripts.</div>
+<?php echo video("https://youtube.com/embed/WOHWmnyZ6zc") ?>
 <div class="section icons">
-<a class="icon" href="https://github.com/pjreddie/darknet" target="_blank">
+<a class="icon" href="https://github.com/salu133445/musegan" target="_blank">
 <ion-icon name="logo-github"></ion-icon>
 <p>GitHub Code</p>
 </a>
 <a class="icon" href="https://arxiv.org/pdf/1709.06298.pdf" target="_blank">
 <ion-icon name="document"></ion-icon>
 <p>Original Paper</p>
+</a>
+<a class="icon" href="https://youtu.be/WOHWmnyZ6zc" target="_blank">
+<ion-icon name="logo-youtube"></ion-icon>
+<p>YouTube Video</p>
 </a>
 </div>
 <?php include "../../footer.php" ?></section><?php include "../../scripts.php" ?>

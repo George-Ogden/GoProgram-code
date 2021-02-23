a0aebbf79769c252876afedd90eb03bda139ff70
George Ogden <George-Ogden@outlook.com>
2021-02-23 15:31:03 +0000

    finished MuseGAN

--- artificial-intelligence/external/musegan.php
+++ artificial-intelligence/external/musegan.php
@@ -5,10 +5,10 @@
 <?php include "../../main.php" ?>
 <?php include "../../meta.php" ?>
 <link rel=stylesheet href=external.css?v=1>
-<meta name=description content="This is an implementation of YOLO to detect objects that has been trained on the COCO dataset or to classify cards using a custom dataset.">
-<meta name=keywords content="yolo object detection">
+<meta name=description content="This is an implementation of MuseGAN to generate melodies based on tracks by The Beatles.">
+<meta name=keywords content="music generation artificial intelligence">
 <meta name=viewport content="width=device-width,initial-scale=1">
-<meta name=subject content="YOLO implmentation">
+<meta name=subject content="MuseGAN implmentation">
 </head><?php include "../../header.php" ?>
 <section class=colour>
 <div class=section id=top>

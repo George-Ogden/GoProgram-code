4fbd3ed713dc24cd514418578e00b988ae9bcff8
George Ogden <George-Ogden@outlook.com>
2021-02-23 14:50:46 +0000

    finished yolo

--- artificial-intelligence/external/yolo.php
+++ artificial-intelligence/external/yolo.php
@@ -8,29 +8,22 @@
 <meta name=description content="This is an implementation of YOLO to detect objects that has been trained on the COCO dataset or to classify cards using a custom dataset.">
 <meta name=keywords content="yolo object detection">
 <meta name=viewport content="width=device-width,initial-scale=1">
-<meta name=subject content="YOLO implemntation">
+<meta name=subject content="YOLO implmentation">
 </head><?php include "../../header.php" ?>
 <section>
 <div class=section id=top>
-<h1 class="colour pad-1 loaded text-left w-25"><span class=highlight>Y</span>ou<br><span class=highlight>O</span>nly<br><span class=highlight>L</span>ook<br><span class=highlight>O</span>nce</h1>
+<h1 class="colour pad-1 loaded text-left"><span class=highlight>Y</span>ou<br><span class=highlight>O</span>nly<br><span class=highlight>L</span>ook<br><span class=highlight>O</span>nce</h1>
 </div>
-<div class="section text flex-row">
-<h3 class="font-italic colour scale pad-1">"Knowledge has no value unless you share it".</h3>
-<p>Therefore, I am giving away Python tutorials to teach people an invaluable skill - programming. Whether you want to speed up mundane tasks or make a career out of writing software, Python is the best language to start with. Hopefully, the code on this website is evidence that I know what I am doing, and I hope that you can develop an awesome skill too.</p>
-</div>
+<?php echo section("YOLO","YOLO is an artificial intelligence algorithm for object detection. As well as classification, YOLO identifies where in an image an object is. It is fast enough to be run in real-time and achieves very a high level of accuracy. The output is a series of bounding boxes with labelled classes, which identify eac object in the image.","/about/images/george ogden.jpg","about") . image("images/yolo.jpg","Custom Playing Card Detector","none") ?>
 <div class="section icons">
-<a class="icon" href="https://github.com/George-Ogden/Understanding-Python" target="_blank">
+<a class="icon" href="https://github.com/pjreddie/darknet" target="_blank">
 <ion-icon name="logo-github"></ion-icon>
 <p>GitHub Code</p>
 </a>
 <a class="icon" href="https://arxiv.org/pdf/2004.10934.pdf" target="_blank">
-<ion-icon name="paper"></ion-icon>
+<ion-icon name="document"></ion-icon>
 <p>Original Paper</p>
 </a>
-</div>
-<div class="section video">
-<h2>Lesson Videos</h2>
-<?php echo video("https://youtube.com/embed/a4wymMoq_kc?list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D") ?>
 </div>
 <?php include "../../footer.php" ?></section><?php include "../../scripts.php" ?>
 

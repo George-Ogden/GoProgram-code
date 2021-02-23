7cc4c0cfd0281b5eda0be64f0be2846d43057aec
George Ogden <George-Ogden@outlook.com>
2021-02-23 10:08:11 +0000

    started on ai

---
+++ artificial-intelligence/external/yolo.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<title>YOLOv4</title>
+<?php include "../../main.php" ?>
+<?php include "../../meta.php" ?>
+<meta name=description content="Colourful projects that have written in Python and are available to download and run or just watch in the browser.">
+<meta name=keywords content="python turtle colours">
+<meta name=viewport content="width=device-width,initial-scale=1">
+<meta name=subject content="Python turtle">
+</head><?php include "../../header.php" ?>
+<section>
+<div class=section id=top>
+<h1 class="colour pad-1">YOLOv4</h1>
+</div>
+<div class="section text flex-row">
+<h3 class="font-italic colour scale pad-1">"Knowledge has no value unless you share it".</h3>
+<p>Therefore, I am giving away Python tutorials to teach people an invaluable skill - programming. Whether you want to speed up mundane tasks or make a career out of writing software, Python is the best language to start with. Hopefully, the code on this website is evidence that I know what I am doing, and I hope that you can develop an awesome skill too.</p>
+</div>
+<div class="section icons">
+<a class="icon" href="https://github.com/George-Ogden/Understanding-Python" target="_blank">
+<ion-icon name="logo-github"></ion-icon>
+<p>GitHub Code</p>
+</a>
+<a class="icon" href="https://www.youtube.com/playlist?list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D" target="_blank">
+<ion-icon name="logo-youtube"></ion-icon>
+<p>YouTube Videos</p>
+</a>
+</div>
+<div class="section video">
+<h2>Lesson Videos</h2>
+<?php echo video("https://youtube.com/embed/a4wymMoq_kc?list=PL1_riyn9sOjcKIAYzo7f8drxD-Yg9La-D") ?>
+</div>
+<?php include "../../footer.php" ?></section><?php include "../../scripts.php" ?>
+
+</html>

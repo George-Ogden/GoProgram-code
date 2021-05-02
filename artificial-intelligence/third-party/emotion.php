b962b2a983543c7de1dea1674f4eb031cdaa84c3
George Ogden <George-Ogden@outlook.com>
2021-05-02 18:32:11 +0100

    finished main page

--- artificial-intelligence/third-party/emotion.php
+++ artificial-intelligence/third-party/emotion.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<title>CycleGAN</title>
+<title>Emotion Detection</title>
 <?php include "../../main.php" ?>
 <?php include "../../meta.php" ?>
 <link rel=stylesheet href=external.css?v=1>
@@ -12,25 +12,43 @@
 </head><?php include "../../header.php" ?>
 <section class=colour>
 <div class=section id=top>
-<h1 class="colour pad-2 loaded text-left"><span class=highlight>Cycle</span>-Consistent<br><span class=highlight>G</span>enerative<br><span class=highlight>A</span>dversarial<br><span class=highlight>N</span>etworks</h1>
-</div>
-<div class="section text"><h2>CycleGAN</h2><p>CycleGAN is a series of GANs that converts unpaired images between domains. In this implementation, I took a dataset of high quality images and partitioned it into male and female faces. Through training, it picked up specific features in the faces to convert male to female and female to male.</div>
-<div class="section image">
-<div class="image my-2"><img src="images/male.jpg" alt="Image Unavailable (Male to Female CycleGAN)"><div class="cover floor"><div class="bottom caption text-white text-center">Male → Female</div></div></div>
-<div class="image my-2"><img src="images/female.jpg" alt="Image Unavailable (Female to Male CycleGAN)"><div class="cover floor"><div class="bottom caption text-white text-center">Female → Male</div></div></div>
+<?php echo image("images/emojis.jpg", "Emojis", "text-darken", ["id" => "top", "text" => "<h1>Emotion Detection</h1>"]); 
+echo section("Emotion Detection","This project consists of two AIs to classify faces into emotions. The first is YOLOv5 and detects the face and the second is RepVGG and decides the emotion on the face. Together, they detect 8 different emotions that are available in the AffectNet dataset and the facial detection dataset used was WIDER FACE.","images/emotions.jpg");
+?>
+<div class="section icons">
+<a class="icon" href="https://github.com/ultralytics/yolov5" target="_blank">
+<ion-icon name="logo-github"></ion-icon>
+<p>YOLOv5 GitHub Code</p>
+</a>
+<a class="icon" href="https://ultralytics.com/yolov5" target="_blank">
+<ion-icon name="link"></ion-icon>
+<p>YOLOv5 Website</p>
+</a>
+<a class="icon" href="https://github.com/DingXiaoH/RepVGG" target="_blank">
+<ion-icon name="logo-github"></ion-icon>
+<p>RepVGG GitHub Code</p>
+</a>
+<a class="icon" href="https://arxiv.org/pdf/2101.03697.pdf" target="_blank">
+<ion-icon name="document"></ion-icon>
+<p>RepVGG Paper</p>
+</a>
 </div>
 <div class="section icons">
-<a class="icon" href="https://github.com/LynnHo/CycleGAN-Tensorflow-2" target="_blank">
-<ion-icon name="logo-github"></ion-icon>
-<p>GitHub Code</p>
-</a>
-<a class="icon" href="https://arxiv.org/pdf/1703.10593.pdf" target="_blank">
+<a class="icon" href="http://mohammadmahoor.com/wp-content/uploads/2017/08/AffectNet_oneColumn-2.pdf" target="_blank">
 <ion-icon name="document"></ion-icon>
-<p>Original Paper</p>
+<p>AffectNet Paper</p>
 </a>
-<a class="icon" href="https://junyanz.github.io/CycleGAN/" target="_blank">
+<a class="icon" href="http://mohammadmahoor.com/affectnet/" target="_blank">
 <ion-icon name="link"></ion-icon>
-<p>Website</p>
+<p>AffectNet Website</p>
+</a>
+<a class="icon" href="https://arxiv.org/pdf/1511.06523.pdf" target="_blank">
+<ion-icon name="document"></ion-icon>
+<p>WIDER FACE Paper</p>
+</a>
+<a class="icon" href="http://shuoyang1213.me/WIDERFACE/" target="_blank">
+<ion-icon name="link"></ion-icon>
+<p>WIDER FACE Website</p>
 </a>
 </div>
 <?php include "../../footer.php" ?></section><?php include "../../scripts.php" ?>

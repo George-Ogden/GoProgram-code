c788657b61ea0c4985cbc29fd729ea12a2cbdd12
George Ogden <George-Ogden@outlook.com>
2021-06-10 15:20:44 +0100

    updated spacing

--- main.php
+++ main.php
@@ -92,7 +92,7 @@ return str_replace_n("page-link", "'page-link selected'", $selector, 1) . "</ul>
 }
 $pos = 1;
 function skills(){
-return image("/images/logos.jpg", "Hand Drawn Logos", "text-overlay", ["text" => "<h2>Skills</h2><p>These are the main skills that I am proficient in, in picture format. If you are looking for a skill that is not listed, feel free to contact me.</p>"]);
+return image("/images/logos.jpg", "Hand Drawn Logos", "text-overlay", ["text" => "<h2 class=pad-1>Skills</h2><p>These are the main skills that I am proficient in, in picture format. If you are looking for a skill that is not listed, feel free to contact me.</p>"]);
 }
 function pph(){
 return "<div class='section image' id=pph><div class=text><h2 class=pb-5>PeoplePerHour</h2><p class=pb-5>PeoplePerHour is the platform that I use for my freelancing. They have a wide range of features that make freelancing easier for buyers and freelancers. These include an escrow account, which keeps your money safe until work is complete, a rating system so that you can find the best freelancers and communications features so that we can keep in touch during work. <a href=https://www.goprogram.co.uk/pph>Have a look at my profile to see some of my previous work.</a></p></div>" . image("/about/images/george ogden.jpg","George Ogden","floor") . "</div>";

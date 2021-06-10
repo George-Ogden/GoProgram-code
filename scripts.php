b759081c3a31226c0f8b85127d811095e456cbbd
George Ogden <George-Ogden@outlook.com>
2021-06-10 16:32:30 +0100

    updated scripts

--- scripts.php
+++ scripts.php
@@ -1,20 +1,9 @@
 <script src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js'></script>
-<script src="/main.js?v=17"></script>
+<script src="/main.js?v=18"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script type="module" src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule="" src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.js"></script><script src=https://www.gstatic.com/firebasejs/7.13.2/firebase-app.js></script>
+<script nomodule="" src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.js"></script>
+<script src=https://www.gstatic.com/firebasejs/7.13.2/firebase-app.js></script>
 <script src=https://www.gstatic.com/firebasejs/7.13.2/firebase-analytics.js></script>
-<script>
-    var firebaseConfig = {
-        apiKey: "AIzaSyBZV2Zn1hwqX54jD9Hb8lNKNESJ_ztFLs8",
-        authDomain: "goprogram-8f3e6.firebaseapp.com",
-        databaseURL: "https://goprogram-8f3e6.firebaseio.com",
-        projectId: "goprogram-8f3e6",
-        storageBucket: "goprogram-8f3e6.appspot.com",
-        messagingSenderId: "418286522925",
-        appId: "1:418286522925:web:954105e701d876248bdea4",
-        measurementId: "G-C5DRZPWGLV"
-    };
-    firebase.initializeApp(firebaseConfig), firebase.analytics()
-</script>
+<script>var firebaseConfig = {apiKey: "AIzaSyBZV2Zn1hwqX54jD9Hb8lNKNESJ_ztFLs8",authDomain: "goprogram-8f3e6.firebaseapp.com",databaseURL: "https://goprogram-8f3e6.firebaseio.com",projectId: "goprogram-8f3e6",storageBucket: "goprogram-8f3e6.appspot.com",messagingSenderId: "418286522925",appId: "1:418286522925:web:954105e701d876248bdea4",measurementId: "G-C5DRZPWGLV"};firebase.initializeApp(firebaseConfig), firebase.analytics()</script>

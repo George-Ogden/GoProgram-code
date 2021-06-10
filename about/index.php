895ee85ec68dc0d1d3f25c1726aab3589d3f3403
George Ogden <George-Ogden@outlook.com>
2021-06-10 16:44:07 +0100

    added pro page

--- about/index.php
+++ about/index.php
@@ -10,9 +10,7 @@
 <meta name=subject content=about>
 </head><?php include "../header.php" ?>
 <section>
-<?php echo image("../images/home.jpg", "Main Image", "text", ["id" => "top", "text" => "<h1 class='colour'>About</h1>"]);  ?>
-
-<?php 
+<?php echo image("../images/home.jpg", "Main Image", "text", ["id" => "top", "text" => "<h1 class='colour'>About</h1>"]);
 echo image("images/code.jpg", "Computer Code", "text-darken", ["text" => "<h2 class=pad-1>Welcome</h2><p>Welcome to GoProgram, a website showcasing programming. Enjoy browsing round the projects and using them, and feel free to use some of the code in your own projects if you wish!</p>"]);
 echo about();
 echo skills(); include "../footer.php" ?><?php include "../scripts.php" ?>
--- pro/index.php
+++ pro/index.php
@@ -10,7 +10,8 @@
 <meta name=subject content="artificial intelligence">
 </head><?php include "../header.php" ?>
 <section class="colour">
-<?php echo image("images/ai.jpg", "Network", "text-darken", ["id" => "top", "text" => "<h1>Professional Projects</h1>"]);
+<?php echo image("images/development.jpg", "Network", "text-darken", ["id" => "top", "text" => "<h1>Professional Projects</h1>"]);
+echo section("Freelancing", "As a freelancer, I love the fact that people pay me to do what I love. To make it easier for myself, I use <a href=/pph>PeoplePerHour</a>, which is a website for freelancers. Here are some previous projects that I've done related to AI (that I'm allowed to share)!","/about/images/george ogden.jpg");
 echo section("YOLO", "YOLO is a leading object detection AI, which generates results with very low latency and high accuracy. For a professional project, I wrote code to detect and group solar panels, but I'm not allowed to show you any of the results! Following that, I played around with this AI to create a custom card detector, as well as using the original model. Why not <a href=yolo>check it out</a>?","images/coco.jpg");
 echo section("PoseNet", "PoseNet is an AI for pose estimation, which assigns keypoints to different parts of the body and then connects them. I integrated this AI with an algorithm to count the number of reps done by an athlete. Why not <a href=posenet>have a look</a>?")?>
 <?php include "../footer.php" ?></section><?php include "../scripts.php" ?>

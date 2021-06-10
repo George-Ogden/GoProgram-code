a7b9d2396f34ea98ba2ab03633528fbd5b33b2e4
George Ogden <George-Ogden@outlook.com>
2021-06-10 16:09:19 +0100

    added scrolling function

--- main.php
+++ main.php
@@ -98,6 +98,6 @@ return image("/images/logos.jpg", "Hand Drawn Logos", "text-overlay", ["text" =>
 // return "<div class='section image' id=pph><div class=text><h2 class=pb-5>PeoplePerHour</h2><p class=pb-5>PeoplePerHour is the platform that I use for my freelancing. They have a wide range of features that make freelancing easier for buyers and freelancers. These include an escrow account, which keeps your money safe until work is complete, a rating system so that you can find the best freelancers and communications features so that we can keep in touch during work. <a href=https://www.goprogram.co.uk/pph>Have a look at my profile to see some of my previous work.</a></p></div>" . image("/about/images/george ogden.jpg","George Ogden","floor") . "</div>";
 // }
 function about(){
-return section("George Ogden","I have a vision to make high-quality, automated technology services available to everyone to make the world function better. I use my experience working in AI and cloud technologies to demonstrate the power of technology to others through clean, yet beautifully crafted code that solves problems effectively.<p>Working as a freelancer allows me to apply my knowledge and creativity to other people's ideas and problems. Hence, I can rise to new challenges and learn new skills in each job that enhance my ability as a developer.","/about/images/george ogden.jpg");
+return section("George Ogden","I am making the world a better place. AI and code are an important part, but it's so much more than just that.","/about/images/george ogden.jpg");
 }
 ?>

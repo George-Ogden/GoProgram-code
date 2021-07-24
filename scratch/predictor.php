5947998c10bc4a8a473bf67cb6e5a62ee3a7f957
George Ogden <George-Ogden@outlook.com>
2021-07-24 19:39:40 +0100

    removed line

--- scratch/predictor.php
+++ scratch/predictor.php
@@ -19,7 +19,6 @@
     <div class="section interactive" id=main>
         <div class="options d-flex flex-column position-relative">
             <?php
-            
             $distances = ["100" => "100m",  "200" => "200m", "300" => "300m", "400" => "400m", "400H" => "400m Hurdles", "800" => "800m", "1000" => "1000m / 1km",  "1500" => "1500m", "Mile" => "1 mile",  "3000" => "3000m / 3km", "3000SC" => "3000m Steeplechase", "5000" => "5000m / 5km", "5M" => "5 miles",  "10000" => "10000m / 10km",  "10M" => "10 miles", "HM" => "Half Marathon",  "Mar" => "Marathon"];
             foreach ($distances as $k => $v) {
                 echo "<div id=$k class='d-flex justify-content-around py-1 time'><div class='w-50 d-flex align-items-center in'><label for=$k class='pr-2 mr-1'>$v</label><input type='checkbox' class='check mr-3 ml-1' name=$k><input name=$k class='in disabled ml-1'></div><div class='w-50 d-flex flex-center out'><input name=$k-predicted class=out disabled></div></div>";

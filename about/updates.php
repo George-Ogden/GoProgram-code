70dbf16cb0c6d9e616ad5f6cd467018fd6ca1567
George Ogden <George-Ogden@outlook.com>
2021-08-13 19:27:40 +0100

    minor update

--- about/updates.php
+++ about/updates.php
@@ -18,4 +18,4 @@
 <?php echo section("Mask-RCNN", "Image Segmentation is a cool field of AI but it be quite user-unfriendly. I've worked with some code and shared some examples <a href=/segmentation>here</a>! You can also rewatch the 100m world record with the background greyed out and the focus on the runners <a href=http://www.youtube.com/watch?v=fRnFHRNIQRs>(very cool)</a>!","/fork/images/maskrcnn.jpg");
 echo section("PB Predictor", "Working out a target time for a running race is very difficult, but this tool can calculate it for you! The <a href=/scratch/pb>PB predictor</a> takes in your current predictor and then gives you targets for other times.","/scratch/images/track.jpg") ?>
 <div class="section text"><h2>Shuffle</h2><p>GoProgram has been around for a while and I thought it was time to move things around. Now, there's a new layout and some of the old projects have been removed to keep everything up to date.</div>
-include "../footer.php" ?></section><?php include "../scripts.php" ?></html>
+<?php include "../footer.php" ?></section><?php include "../scripts.php" ?></html>

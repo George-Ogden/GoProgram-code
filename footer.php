24e3fb96252ccb0993f3bdc20bcec41168c0575b
George Ogden <George-Ogden@outlook.com>
2022-04-04 09:34:09 +0100

    updated footer

--- footer.php
+++ footer.php
@@ -5,6 +5,6 @@ echo "<div  class='$lower icon'><a href=/$lower target=_blank data-toggle=toolti
 }
 icon("YouTube");
 icon("GitHub");
-icon("CodeChef");
+icon("PeoplePerHour");
 icon("LinkedIn"); ?>
 </footer>

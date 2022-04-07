bf404b352b9ad34b1c7d19bda5b9475fa41feac9
George Ogden <George-Ogden@outlook.com>
2022-04-07 12:43:22 +0100

    created files

---
+++ scratch/tictactoe.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<title>Tic Tac Toe</title>
+<?php include "../main.php" ?>
+<link rel=stylesheet href=tictactoe.css?v=1>
+<?php include "../meta.php" ?>
+<meta name=description content="">
+<meta name=keywords content="">
+<meta name=viewport content="width=device-width,initial-scale=1">
+<meta name=subject content="">
+</head><?php include "../header.php" ?>
+<section>
+<div class=section id=top>
+<h1 class="colour pad-1">Tic Tac Toe</h1>
+</div>
+<div class="main section interactive"><table id=board>
+<?php
+for ($i = 0; $i < 5; $i++) {
+echo "<tr>";
+for ($j = 0; $j < 5; $j++) {
+echo "<td class=box pos=".strval($i * 5 + $j + 1)." col=$j row=$i><circle></circle></td>";
+}
+echo "</tr>";
+} ?>
+</table>
+    <div class="cover d-none w-100 h-100">
+        <div id="spinner" class="m-auto"></div>
+    </div></div>
+<?php include "../footer.php"?></section><?php include "../scripts.php" ?>
+<script src=tictactoe.js></script>

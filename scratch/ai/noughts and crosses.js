efd0f4d17b7bf2655df3d017727b88c27a01f7f2
George Ogden <George-Ogden@outlook.com>
2022-05-03 22:13:02 +0100

    added ending

--- scratch/ai/noughts and crosses.js
+++ scratch/ai/noughts and crosses.js
@@ -1,1 +1,132 @@
-class Noughts_and_Crosses extends Umpire{constructor(t=3,i=3){super(t),this.win=i}check_state(t=this.board){for(let i=0;i<this.size;i++)for(let e=0;e<this.size-this.win+1;e++){if(0==t.data[i][e])continue;let s=t.data[i][e];for(let a=1;a<this.win;a++)if(t.data[i][e+a]!=s){s=0;break}if(0!=s)return s}for(let i=0;i<this.size-this.win+1;i++)for(let e=0;e<this.size;e++){if(0==t.data[i][e])continue;let s=t.data[i][e];for(let a=1;a<this.win;a++)if(t.data[i+a][e]!=s){s=0;break}if(0!=s)return s}for(let i=0;i<this.size-this.win+1;i++)for(let e=0;e<this.size-this.win+1;e++){if(0==t.data[i][e])continue;let s=t.data[i][e];for(let a=1;a<this.win;a++)if(t.data[i+a][e+a]!=s){s=0;break}if(0!=s)return s}for(let i=this.win-1;i<this.size;i++)for(let e=0;e<this.size-this.win+1;e++){if(0==t.data[i][e])continue;let s=t.data[i][e];for(let a=1;a<this.win;a++)if(t.data[i-a][e+a]!=s){s=0;break}if(0!=s)return s}return 0}}
+const delay = ms => new Promise(res => setTimeout(res, ms));
+class Noughts_and_Crosses extends Umpire {
+	constructor(t = 3, i = 3) {
+		super(t), (this.win = i);
+	}
+	check_state(t = this.board) {
+		for (let i = 0; i < this.size; i++)
+			for (let e = 0; e < this.size - this.win + 1; e++) {
+				if (0 == t.data[i][e]) continue;
+				let s = t.data[i][e];
+				for (let a = 1; a < this.win; a++)
+					if (t.data[i][e + a] != s) {
+						s = 0;
+						break;
+					}
+				if (0 != s) return s;
+			}
+		for (let i = 0; i < this.size - this.win + 1; i++)
+			for (let e = 0; e < this.size; e++) {
+				if (0 == t.data[i][e]) continue;
+				let s = t.data[i][e];
+				for (let a = 1; a < this.win; a++)
+					if (t.data[i + a][e] != s) {
+						s = 0;
+						break;
+					}
+				if (0 != s) return s;
+			}
+		for (let i = 0; i < this.size - this.win + 1; i++)
+			for (let e = 0; e < this.size - this.win + 1; e++) {
+				if (0 == t.data[i][e]) continue;
+				let s = t.data[i][e];
+				for (let a = 1; a < this.win; a++)
+					if (t.data[i + a][e + a] != s) {
+						s = 0;
+						break;
+					}
+				if (0 != s) return s;
+			}
+		for (let i = this.win - 1; i < this.size; i++)
+			for (let e = 0; e < this.size - this.win + 1; e++) {
+				if (0 == t.data[i][e]) continue;
+				let s = t.data[i][e];
+				for (let a = 1; a < this.win; a++)
+					if (t.data[i - a][e + a] != s) {
+						s = 0;
+						break;
+					}
+				if (0 != s) return s;
+			}
+		return 0;
+	}
+    async find(){
+        await delay(1000)
+        for (let i = 0; i < this.size; i++){
+            for (let j = 0; j < this.size - this.win + 1; j++){
+                if (!this.board.data[i][j]) continue;
+                let same = true;
+                for (let k = 1; k < this.win; k++){
+                    if (this.board.data[i][j+k] != this.board.data[i][j]){
+                        same = false;
+                        break;
+                    }
+                }
+                if (same){
+                    for (let k = 0; k < this.win; k++){
+                        let piece = $(`[row=${i}][col=${j+k}] circle`)
+                        piece.css("background",`radial-gradient(green, ${piece.css("background-color")})`)
+                    }
+                    return
+                }
+            }
+        }
+        for (let i = 0; i < this.size; i++){
+            for (let j = 0; j < this.size - this.win + 1; j++){
+                if (!this.board.data[j][i]) continue;
+                let same = true;
+                for (let k = 1; k < this.win; k++){
+                    if (this.board.data[j+k][i] != this.board.data[j][i]){
+                        same = false;
+                        break;
+                    }
+                }
+                if (same){
+                    for (let k = 0; k < this.win; k++){
+                        let piece = $(`[row=${j+k}][col=${i}] circle`)
+                        piece.css("background",`radial-gradient(green, ${piece.css("background-color")})`)
+                    }
+                    return
+                }
+            }
+        }
+        for (let i = 0; i < this.size - this.win + 1; i++){
+            for (let j = 0; j < this.size - this.win + 1; j++){
+                if (!this.board.data[i][j]) continue;
+                let same = true;
+                for (let k = 1; k < this.win; k++){
+                    if (this.board.data[i+k][j+k] != this.board.data[i][j]){
+                        same = false;
+                        break;
+                    }
+                }
+                if (same){
+                    for (let k = 0; k < this.win; k++){
+                        let piece = $(`[row=${i+k}][col=${j+k}] circle`)
+                        piece.css("background",`radial-gradient(var(--light), ${piece.css("background-color")})`)
+                    }
+                    return
+                }
+            }
+        }
+        for (let i = 0; i < this.size - this.win + 1; i++){
+            for (let j = this.win - 1; j < this.size; j++){
+                if (!this.board.data[i][j]) continue;
+                let same = true;
+                for (let k = 1; k < this.win; k++){
+                    if (this.board.data[i+k][j-k] != this.board.data[i][j]){
+                        same = false;
+                        break;
+                    }
+                }
+                if (same){
+                    for (let k = 0; k < this.win; k++){
+                        let piece = $(`[row=${i+k}][col=${j-k}] circle`)
+                        piece.css("background",`radial-gradient(green, ${piece.css("background-color")})`)
+                    }
+                    return
+                }
+            }
+        }
+    }
+}
--- scratch/connect4.js
+++ scratch/connect4.js
@@ -1,1 +1,142 @@
-const size=7,umpire=new Noughts_and_Crosses(7,4),pieces={0:".",1:"X","-1":"O",".":0,X:1,O:-1},player={move:async function(t){w=t.rss()%2*2-1;let o=await this.request(Matrix.flatten(t).data[0].map(t=>pieces[t]).join(""));if(200==o.status)return Matrix.multiply(Matrix.resize(Matrix.fromArray([o.responseText.toUpperCase().split("").map(t=>pieces[t])]),7,7),w)},request:function(t){return new Promise(function(t){let o=new XMLHttpRequest;o.open("GET",`https://api.goprogram.ai/games/connect4?board=${history}`),o.onload=function(){200==this.status&&t(o)},o.send()})}};let move_promise,thinking=!1,history="";function endGame(){finishMove(),thinking=!0}function finishMove(){thinking=!1,$(".cover").addClass("d-none"),$(".cover").removeClass("darken d-flex")}async function displayBoard(t){for(let o=0;o<t.rows;o++)for(let e=0;e<t.cols;e++){let r=t.data[o][e];0!=r&&($(`[row=${o}][col=${e}].box circle`).css({background:r<0?"red":"blue",opacity:1}),$(`[row=${o}][col=${e}].box`).css("background","var(--primary-light)"),$(`[row=${o}][col=${e}].box`).hasClass("taken")||$(`[row=${o}][col=${e}].box`).addClass("taken"))}}async function animateMove(t,o){history+=String(o),t=parseInt(t),distance=$("#board").offset().top-$(`[row=${t}][col=${o}].box circle`).offset().top,$(`[row=${t}][col=${o}].box circle`).css("transform",`translate(0,${distance}px`),await new Promise(t=>setTimeout(t,100)),$(`[row=${t}][col=${o}].box circle`).css("transition",`transform ${Math.sqrt(-1*distance)/100}s cubic-bezier(.3,0,.69,.17)`),$(`[row=${t}][col=${o}].box circle`).css("transform","translate(0,0)")}function resize(){$("#board").height($("#board").width())}$("#board").on("mouseenter",function(){$(this).off("mouseenter"),umpire.challenge(player)}),$(window).resize(resize),resize(),$("td.box").on("mouseover",function(){!thinking&&$(`[col=${$(this).attr("col")}]:not(.taken)`).length>0&&($(`[col=${$(this).attr("col")}]:not(.taken)`).css("background","var(--base)"),$(`[row=${Object.values($(`[col=${$(this).attr("col")}]:not(.taken)`)).reduce((t,o)=>Math.max(t,$(o).attr("row")||0),0)}][col=${$(this).attr("col")}] circle`).css({opacity:"0.3",background:"red"}))}),$("td.box").on("click",function(){!thinking&&$(`[col=${$(this).attr("col")}]:not(.taken)`).length>0&&(move_promise([Object.values($(`[col=${$(this).attr("col")}]:not(.taken)`)).reduce((t,o)=>Math.max(t,$(o).attr("row")||0),0),$(this).attr("col")]),thinking=!0,$(".cover").removeClass("d-none"),$(".cover").addClass("darken d-flex"),$(".box:not(.taken) circle").css("background","transparent"),$(".box").css("background","var(--primary-light)"))}),$("td.box").on("mouseout",function(){$(`[col=${$(this).attr("col")}]:not(.taken)`).length>0&&($(`[col=${$(this).attr("col")}]:not(.taken)`).css("background","var(--primary-light)"),$(`[row=${Object.values($(`[col=${$(this).attr("col")}]:not(.taken)`)).reduce((t,o)=>Math.max(t,$(o).attr("row")||0),0)}][col=${$(this).attr("col")}] circle`).css({background:"transparent"}))});
+const size = 7,
+	umpire = new Noughts_and_Crosses(7, 4),
+	pieces = { 0: ".", 1: "X", "-1": "O", ".": 0, X: 1, O: -1 },
+	player = {
+		move: async function (t) {
+			w = (t.rss() % 2) * 2 - 1;
+			let o = await this.request(
+				Matrix.flatten(t)
+					.data[0].map((t) => pieces[t])
+					.join("")
+			);
+			if (200 == o.status)
+				return Matrix.multiply(
+					Matrix.resize(
+						Matrix.fromArray([
+							o.responseText
+								.toUpperCase()
+								.split("")
+								.map((t) => pieces[t]),
+						]),
+						7,
+						7
+					),
+					w
+				);
+		},
+		request: function (t) {
+			return new Promise(function (t) {
+				let o = new XMLHttpRequest();
+				o.open(
+					"GET",
+					`https://api.goprogram.ai/games/connect4?board=${history}`
+				),
+					(o.onload = function () {
+						200 == this.status && t(o);
+					}),
+					o.send();
+			});
+		},
+	};
+let move_promise,
+	thinking = !1,
+	history = "";
+function endGame() {
+	finishMove(), (thinking = !0);
+    umpire.find();
+}
+function finishMove() {
+	(thinking = !1),
+		$(".cover").addClass("d-none"),
+		$(".cover").removeClass("darken d-flex");
+}
+async function displayBoard(t) {
+	for (let o = 0; o < t.rows; o++)
+		for (let e = 0; e < t.cols; e++) {
+			let r = t.data[o][e];
+			0 != r &&
+				($(`[row=${o}][col=${e}].box circle`).css({
+					background: r < 0 ? "red" : "blue",
+					opacity: 1,
+				}),
+				$(`[row=${o}][col=${e}].box`).css(
+					"background",
+					"var(--primary-light)"
+				),
+				$(`[row=${o}][col=${e}].box`).hasClass("taken") ||
+					$(`[row=${o}][col=${e}].box`).addClass("taken"));
+		}
+}
+async function animateMove(t, o) {
+	(history += String(o)),
+		(t = parseInt(t)),
+		(distance =
+			$("#board").offset().top -
+			$(`[row=${t}][col=${o}].box circle`).offset().top),
+		$(`[row=${t}][col=${o}].box circle`).css(
+			"transform",
+			`translate(0,${distance}px`
+		),
+		await new Promise((t) => setTimeout(t, 100)),
+		$(`[row=${t}][col=${o}].box circle`).css(
+			"transition",
+			`transform ${
+				Math.sqrt(-1 * distance) / 100
+			}s cubic-bezier(.3,0,.69,.17)`
+		),
+		$(`[row=${t}][col=${o}].box circle`).css("transform", "translate(0,0)");
+}
+function resize() {
+	$("#board").height($("#board").width());
+}
+$("#board").on("mouseenter", function () {
+	$(this).off("mouseenter"), umpire.challenge(player);
+}),
+	$(window).resize(resize),
+	resize(),
+	$("td.box").on("mouseover", function () {
+		!thinking &&
+			$(`[col=${$(this).attr("col")}]:not(.taken)`).length > 0 &&
+			($(`[col=${$(this).attr("col")}]:not(.taken)`).css(
+				"background",
+				"var(--base)"
+			),
+			$(
+				`[row=${Object.values(
+					$(`[col=${$(this).attr("col")}]:not(.taken)`)
+				).reduce(
+					(t, o) => Math.max(t, $(o).attr("row") || 0),
+					0
+				)}][col=${$(this).attr("col")}] circle`
+			).css({ opacity: "0.3", background: "red" }));
+	}),
+	$("td.box").on("click", function () {
+		!thinking &&
+			$(`[col=${$(this).attr("col")}]:not(.taken)`).length > 0 &&
+			(move_promise([
+				Object.values(
+					$(`[col=${$(this).attr("col")}]:not(.taken)`)
+				).reduce((t, o) => Math.max(t, $(o).attr("row") || 0), 0),
+				$(this).attr("col"),
+			]),
+			(thinking = !0),
+			$(".cover").removeClass("d-none"),
+			$(".cover").addClass("darken d-flex"),
+			$(".box:not(.taken) circle").css("background", "transparent"),
+			$(".box").css("background", "var(--primary-light)"));
+	}),
+	$("td.box").on("mouseout", function () {
+		$(`[col=${$(this).attr("col")}]:not(.taken)`).length > 0 &&
+			($(`[col=${$(this).attr("col")}]:not(.taken)`).css(
+				"background",
+				"var(--primary-light)"
+			),
+			$(
+				`[row=${Object.values(
+					$(`[col=${$(this).attr("col")}]:not(.taken)`)
+				).reduce(
+					(t, o) => Math.max(t, $(o).attr("row") || 0),
+					0
+				)}][col=${$(this).attr("col")}] circle`
+			).css({ background: "transparent" }));
+	});

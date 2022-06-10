3c06d195d77f17f6f1bc4a8c82ce460f1188c6d7
George Ogden <George-Ogden@outlook.com>
2022-06-10 19:37:42 +0100

    updated updates

--- about/updates.php
+++ about/updates.php
@@ -15,8 +15,9 @@
 <div class="section text">
 <p>Every time a new web page is launched, it will show up here so that you can see what is new!</p>
 </div>
-<?php echo section("Bitcoin Bot", "Bitcoin seems to be taking over the world at the moment, but it's not as unpredictable as you'd think. <a href=/pro/bitcoin>This bot</a> manages to find patterns in the data to make profitable investments!","/pro/images/bitcoin.jpg");
+<?php 
+echo section("Abstract Art", "After a hardware upgrade, I decided to train a fresh StyleGAN2-ADA model on a dataset I compiled of abstract art. <a href=/fork/abstract>The model</a> creates realistic abstract art with its own distinctive style!","/fork/images/abstract-side.jpg");
+echo section("Bitcoin Bot", "Bitcoin seems to be taking over the world at the moment, but it's not as unpredictable as you'd think. <a href=/pro/bitcoin>This bot</a> manages to find patterns in the data to make profitable investments!","/pro/images/bitcoin.jpg");
 echo section("Tic-Tac-Toe", "As part of my research on games, I decided to create an AI that would always win at tic-tac-toe. To extend the challenge, I moved up from a 3x3 grid to a 5x5 one and you can <a href=/ai/tictactoe/>play against the result</a>!","/scratch/images/tictactoe.jpg");
-echo section("U-Net", "Another interesting project that I've recently finished is vein detection in leaves. To accomplish this, I used U-Net - an image segmentation model designed for medical uses - and you can <a href=/ai/vein>view the results</a>!","/pro/images/vein.jpg");
 ?>
 <?php include "../footer.php" ?></section><?php include "../scripts.php" ?></html>

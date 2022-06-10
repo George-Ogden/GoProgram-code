7e3408db155dbdf69637d31c7bbb1aa728be3ea0
George Ogden <George-Ogden@outlook.com>
2022-06-10 19:21:33 +0100

    added links

--- fork/abstract.php
+++ fork/abstract.php
@@ -13,18 +13,14 @@
 <section class=colour>
 <div class=section id=top>
 <?php echo image("images/abstract-cover.jpg", "AI Generated Abstract Art", "text-darken", ["id" => "top", "text" => "<h1>Abstract Art</h1>"]); 
-echo section("Abstract Art","This AI has learned to extract the common patterns that we create in abstract art and then place them into its own version of art. It has seen 1000s of images from a range of artists, yet still has its own distinctive style.","images/abstract-side.jpg") . image("images/abstract-base.jpg","AI Generated Abstract Art","none") ?>
+echo section("Abstract Art","This AI (StyleGAN2-ADA) has learned to extract the common patterns that we create in abstract art and then place them into its own version of art. It has seen 1000s of images from a range of artists, yet still has its own distinctive style.","images/abstract-side.jpg") . image("images/abstract-base.jpg","AI Generated Abstract Art","none") ?>
 
 <div class="section icons">
-<a class="icon" href="" target="_blank">
+<a class="icon" href="https://github.com/NVlabs/stylegan2-ada-pytorch" target="_blank">
 <ion-icon name="logo-github"></ion-icon>
 <p>GitHub Code</p>
 </a>
-<a class="icon" href="" target="_blank">
-<ion-icon name="link"></ion-icon>
-<p>Website</p>
-</a>
-<a class="icon" href="" target="_blank">
+<a class="icon" href="https://arxiv.org/pdf/2006.06676.pdf" target="_blank">
 <ion-icon name="document"></ion-icon>
 <p>Paper</p>
 </a>

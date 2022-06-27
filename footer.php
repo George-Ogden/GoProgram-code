43d351f42103db65b609432de843c7814401406d
George Ogden <George-Ogden@outlook.com>
2022-06-27 17:05:26 +0100

    added upwork logo to footer

--- footer.php
+++ footer.php
@@ -6,5 +6,6 @@ echo "<div  class='$lower icon'><a href=/$lower target=_blank data-toggle=toolti
 icon("YouTube");
 icon("GitHub");
 icon("PeoplePerHour");
+icon("Upwork");
 icon("LinkedIn"); ?>
 </footer>

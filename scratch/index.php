498fca7d7a36f017e23b6162d48844322ac1aa50
George Ogden <38294960+George-Ogden@users.noreply.github.com>
2022-08-05 18:43:03 +0100

    updated index page

--- scratch/index.php
+++ scratch/index.php
@@ -12,9 +12,11 @@
 <section class="colour">
 <?php echo image("images/brain.jpg", "Intelligence", "text-darken", ["id" => "top", "text" => "<h1>AI from Scratch</h1>"]);
 echo image("images/network.jpg", "Neural Network", "text", ["text" => "<p>Take a look at some AI written from scratch that you can play with directly in your browser!</p>"]);
-echo section("Connect 4", "<a href=connect4>Play against an AI</a> at a game of connect 4. It has trained by playing itself millions of times after only being taught the rules of the game.","images/connect4.jpg");
-echo section("Dinosaur Generator", "The dinosaur generator uses an n-gram-like algorithm to generate realistic dinosaur names. <a href=/ai/dinosaurs>Check it out</a> and have a play with the cool feature.","images/dinosaur.jpg");
 echo section("Tic-Tac-Toe", "The original 3x3 game of tic-tac-toe is trivial but on a larger grid, this is no longer the case. With a 5x5 grid and the aim of getting 4 in any direction, this advanced version gives you the opportunity to <a href=/ai/tictactoe>play against an AI</a>.","images/tictactoe.jpg");
+echo section("Connect 4", "<a href=connect4>Play against an AI</a> at a game of connect 4. It has trained by playing doing millions of simulations to determine the best move in a position so that it can win as quickly as possible.","images/connect4.jpg");?>
+<div class="section text"><h2>Squares</h2><p>After AlphaZero's greatness at go, chess and almost every other game, I thought it would be worth putting it to the test in this classic game. Completing a squares wins a point, but can you <a href=squares>complete more than the AI</a>?</div>
+<?php
+echo section("Dinosaur Generator", "The dinosaur generator uses an n-gram-like algorithm to generate realistic dinosaur names. <a href=/ai/dinosaurs>Check it out</a> and have a play with the cool feature.","images/dinosaur.jpg");
 echo section("Hangman Solver", "Usually, hangman is about guessing another person's word. But here, the artificial intelligence tries to guess your word, and it learns with every attempt! Why not <a href=/ai/hangman>have a play with it</a>?","images/hangman.jpg");
 echo section("PB Predictor", "Working out a target time for a running race is very difficult, but this tool can calculate it for you! The <a href=/scratch/pb>PB predictor</a> takes in your current predictor and then gives you targets for other times.","images/track.jpg");
 include "../footer.php" ?></section><?php include "../scripts.php" ?>
